4b90b3c0cc98c05d03a396cf631bfccf6c3f5721
HBM18 <hermesonbastos@gmail.com>
2022-07-22 22:45:34 -0300

    add workflows

---
+++ src/components/Resume/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1120px;
+  width: 98%;
+  margin: 0 auto;
+  display: flex;
+  gap: 20px;
+  margin-top: -50px;
+  justify-content: space-around;
+
+  @media (max-width: 470px){
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+  }
+`;
